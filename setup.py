0c36b004202584f4228ab06a34262b2e3ebb0f21
David Dworken <david@daviddworken.com>
2016-02-02 07:59:13 -0500

    Increment to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SlopeFields',
-    version='0.1.0',
+    version='0.1.1',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['SlopeFields'],
