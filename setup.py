96eb462d1084bfe7ccfdd2dae2305799d1af31e7
David Dworken <david@daviddworken.com>
2016-02-03 15:21:37 -0500

    Added --output flag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SlopeFields',
-    version='0.1.2',
+    version='0.2.0',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['SlopeFields'],
